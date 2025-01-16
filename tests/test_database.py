3e49b5f7958a95cee99a51ebc43e05792a956bdb
Valentina Pakhatinskaya <aalentinaa@gmail.com>
2025-01-16 23:59:33 +0500

    added: helpers.py file, cycle moved to helpers.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,3 +1,4 @@
+from helpers import filter_ingredients_by_type
 from praktikum.database import Database
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
@@ -14,14 +15,14 @@ class TestDatabase:
         assert len(available_ingredients) == 6
 
     def test_get_available_sauces_from_ingredients(self):
-        database_sauces_ingredients = Database()
-        available_sauces_in_ingredients = database_sauces_ingredients.available_ingredients()
-        type_sauce = [i for i in available_sauces_in_ingredients if i.get_type() == INGREDIENT_TYPE_SAUCE]
-        assert len(type_sauce) == 3
+        database = Database()
+        available_ingredients = database.available_ingredients()
+        sauces = filter_ingredients_by_type(available_ingredients, INGREDIENT_TYPE_SAUCE)
+        assert len(sauces) == 3
 
     def test_get_available_fillings_from_ingredients(self):
-        database_fillings_ingredients = Database()
-        available_fillings_in_ingredients = database_fillings_ingredients.available_ingredients()
-        type_filling = [i for i in available_fillings_in_ingredients if i.get_type() == INGREDIENT_TYPE_FILLING]
-        assert len(type_filling) == 3
+        database = Database()
+        available_ingredients = database.available_ingredients()
+        fillings = filter_ingredients_by_type(available_ingredients, INGREDIENT_TYPE_FILLING)
+        assert len(fillings) == 3
 
